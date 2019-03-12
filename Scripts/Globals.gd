18e53f0bcca928a605dc3773dcd52aab25aa7eec
Totalfreak <dgkartin@live.dk>
2019-03-12 12:50:05 +0100

    Added kitchens to main

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -26,6 +26,7 @@ func _physics_process(delta):
 
 func check_if_player_in_light(position, length):
 	var space_state = get_world_2d().direct_space_state
+	print(position)
 	#print((player.global_position - global_position).normalized())
 	# Fire ray from the light position towards player position, at the player physics layer
 	result = space_state.intersect_ray(position, player.global_position, [], 1)
@@ -42,6 +43,7 @@ func enter_ventilation(enter_pos):
 	player.set_collision_mask_bit(0, false)
 	player.set_collision_layer_bit(3, true)
 	player.set_collision_mask_bit(3, true)
+	player.get_node("Camera2D").current = true
 	player.global_position = enter_pos
 	player.inside_vent = true
 	player.get_node("Ventilation Light2D").visible = true
