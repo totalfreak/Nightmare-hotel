9e61291da88ffcaed4c0c85df871504075002759
Totalfreak <dgkartin@live.dk>
2019-03-12 14:52:26 +0100

    Fixed kitchen gaps

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -33,7 +33,7 @@ func check_if_player_in_light(position, length):
 	
 	#print(result)
 	if result:
-		if result.collider == player and position.distance_to(player.global_position)  / 35 < length:
+		if result.collider == player and position.distance_to(player.global_position)  / 25 < length:
 			ray_draw_list.append([result.position, position])
 			update()
 			#print(result.position.distance_to(position))
