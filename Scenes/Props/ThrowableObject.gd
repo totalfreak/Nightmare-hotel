25e746ee7c0d0739d0f3896948ea6df292ee2e41
Totalfreak <dgkartin@live.dk>
2019-03-12 14:06:19 +0100

    Made table in kitchen be in throwable physics layer

--- Scenes/Props/ThrowableObject.gd
+++ Scenes/Props/ThrowableObject.gd
@@ -4,7 +4,7 @@ var OGowner
 var playerInside = false
 var isPickedUp = false
 var throwOffset = Vector2(0,0)
-var throwForce = 20000
+var throwForce = 30000
 
 func _ready():
 	OGowner = self.get_owner()
