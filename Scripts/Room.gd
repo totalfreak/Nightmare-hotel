fc0ccf1d7092421d319d8f86f8df6da2eb8a094f
Totalfreak <dgkartin@live.dk>
2019-03-26 10:29:35 +0100

    Fixed player layer

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -16,5 +16,13 @@ func _process(delta):
 
 func _on_Area2D_body_entered(body):
 	if body == Globals.player:
-		get_node("Camera2D").make_current()
+		#get_node("Camera2D").make_current()
+		$"Not In Room Dark".modulate = Color(1,1,1,.7)
 	pass
+
+
+
+func _on_Area2D_body_exited(body):
+	if body == Globals.player:
+		$"Not In Room Dark".modulate = Color(1,1,1,0)
+	pass # replace with function body
