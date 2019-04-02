516f1f276ec03720ba6431a10a20f17ddb89cd12
Totalfreak <dgkartin@live.dk>
2019-04-02 10:58:19 +0200

    Fix collision polygons for rooms

--- Scenes/Props/ThrowableObject.gd
+++ Scenes/Props/ThrowableObject.gd
@@ -25,25 +25,31 @@ func _process(delta):
 		isPickedUp = false
 		Globals.player.handsFull = false
 		throwObject(delta)
-	pass
+	
+	if self.is_sleeping():
+		print("Sleeping")
+		set_collision_layer_bit(1, false)
+		set_collision_mask_bit(1, false)
 
 func throwObject(delta):
 	var throwDir = (get_global_mouse_position() - self.global_position).normalized() * (throwForce * (mass )) * delta
 	var tempGlobal = self.global_position
 	Globals.player.get_node("ThrowableContainer").remove_child(self)
 	OGowner.add_child(self)
-	self.global_position = tempGlobal 
+	
 	self.set_owner(OGowner)
 	self.mode = RigidBody2D.MODE_RIGID
+	self.global_position = tempGlobal + (get_global_mouse_position() - tempGlobal).normalized() * 50
 	apply_impulse(throwOffset, throwDir)
 	set_collision_layer_bit(3, true)
 	set_collision_mask_bit(3, true)
+	set_collision_layer_bit(1, true)
+	set_collision_mask_bit(1, true)
 	set_collision_layer_bit(4, true)
 	set_collision_mask_bit(4, true)
 	pass
 
 func pickUpObject():
-	
 	self.get_parent().remove_child(self)
 	Globals.player.get_node("ThrowableContainer").add_child(self)
 	self.set_owner(Globals.player.get_node("ThrowableContainer"))
@@ -51,6 +57,8 @@ func pickUpObject():
 	self.mode = RigidBody2D.MODE_KINEMATIC
 	set_collision_layer_bit(3, false)
 	set_collision_mask_bit(3, false)
+	set_collision_layer_bit(1, false)
+	set_collision_mask_bit(1, false)
 	set_collision_layer_bit(4, false)
 	set_collision_mask_bit(4, false)
 	pass
@@ -68,3 +76,10 @@ func _on_PlayerEntered_body_exited(body):
 		playerInside = false
 		my_sprite.get_material().set_shader_param("shouldOutline", false)
 	pass 
+
+
+func _on_ThrowableObject_sleeping_state_changed():
+	if self.is_sleeping():
+		print("Sleeping")
+		set_collision_layer_bit(1, false)
+		set_collision_mask_bit(1, false)
